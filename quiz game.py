5b4e0ea4635034512d8cbf65eb96ea83e9cb1792
shivaaradhya1807-del <shivaaradhya1807@gmail.com>
2026-05-12 12:26:53 +0530

    Add files via upload

---
+++ quiz game.py
@@ -0,0 +1,9 @@
+print("How many oceans there in the world")
+a = 3
+b = 5
+c = 15
+answer = input(" enter answer ")
+if answer == "5":
+    print("Great you got it correct!")
+else:
+    print("wrong!")
