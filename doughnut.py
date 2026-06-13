fb032a0238739e702fcf601b90ef664e1ba127cc
shivaaradhya1807-del <shivaaradhya1807@gmail.com>
2026-06-13 21:47:18 +0530

    Add files via upload

---
+++ doughnut.py
@@ -0,0 +1,27 @@
+import turtle
+import random
+
+screen = turtle.Screen()
+screen.bgcolor("pink")
+screen.setup(600, 600)
+
+t = turtle.Turtle()
+t.pensize(100)
+t.pencolor("lightblue")
+t.circle(90)
+
+colors = ["red", "yellow", "blue", "green", "purple", "white", "orange"]
+
+for i in range(30):
+    t.forward(15)
+    t.left(15)
+
+    t.pendown()
+    t.pensize(5)
+    t.pencolor(random.choice(colors))
+    t.forward(10)
+    t.penup()
+
+turtle.done()
+
+
