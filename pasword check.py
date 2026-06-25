0ad49c17b6920ec3a9f7cd86a6a6bdec9ff3a63a
shivaaradhya1807-del <shivaaradhya1807@gmail.com>
2026-06-25 13:19:34 +0530

    Add files via upload

---
+++ pasword check.py
@@ -0,0 +1,12 @@
+password=int(input(" \n Enter password 1 2 3 4 5 6 7 8 9 0"))
+
+if password==6721:
+    print("\n ====== PHONE UNLOCKED 🔓======")
+else:
+    print(" \n Wrong password ❌!!")
+    print("Try again")
+    print("   .     .     .    .")
+    print("   1     2     3 ")
+    print("   4     5     6 ")
+    print("   7     8     9 ")
+    print("         0       ")
