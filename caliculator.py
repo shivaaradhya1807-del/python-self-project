6d8db2ef838f1feb679d33c3eea5cddff9b9dacc
shivaaradhya1807-del <shivaaradhya1807@gmail.com>
2026-06-25 13:19:14 +0530

    Add files via upload

---
+++ caliculator.py
@@ -0,0 +1,13 @@
+print("========WELCOME TO NO DECIMAL CALICULATOR============")
+
+a=int(input("Enter a number = 🔢"))
+b=int(input("Enter another number = 🔢"))
+
+print(" a + b =",a + b)
+
+print(" a - b =",a - b)
+
+print(" a * b =",a *b)
+
+print(" a  /b =",a /b)
+
