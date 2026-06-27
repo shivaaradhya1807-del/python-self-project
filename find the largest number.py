4ab92d59a112d05324d6183a9428339039f764fa
shivaaradhya1807-del <shivaaradhya1807@gmail.com>
2026-06-27 09:35:25 +0530

    Add files via upload

---
+++ find the largest number.py
@@ -0,0 +1,9 @@
+number_1=int(input("Enter your number :-🔢"))
+number_2=int(input("Enter another number:-"))
+
+if number_1 > number_2 :
+    print (number_1,(">"),number_2)
+elif number_2 > number_1:
+    print(number_2,(">"),number_1)
+else :
+    print(number_1,("="),number_2)
